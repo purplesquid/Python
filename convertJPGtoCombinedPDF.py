083296aa79dc2c697be29ad90007052401b4a986
purplesquid <purplesquid@users.noreply.github.com>
2023-04-16 22:22:57 -0400

    Adds all images in a folder to one pdf

--- convertJPGtoCombinedPDF.py
+++ convertJPGtoCombinedPDF.py
@@ -1,31 +1,20 @@
 import os
 from PIL import Image
 
-directory = ""
-fullPath = "" + directory
-
-def combine(name, jpg, chapter):
-    pdfPath = "" + directory + r"/"
-    pdfName = pdfPath + name + ".pdf"
+def main():
+    imagePath = r""
+    savePath = r""
+    name = ""
+    pdfPath = savePath + "\\" + name + ".pdf"
     
     images = [
-        Image.open(chapter + r"/" + str(f) + ".jpg")
-        for f in range(1,jpg+1)
+        Image.open(imagePath + "\\" + f)
+        for f in os.listdir(imagePath)
     ]
 
     images[0].save(
-        pdfName, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
+        pdfPath, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
     )
-
-def main():
-    for folder in os.listdir(fullPath):
-        chapter = os.path.join(fullPath,folder)
-        jpgCount = 0
-        
-        for file in os.listdir(chapter):
-            jpgCount = jpgCount + 1
-            
-        combine(folder, jpgCount, chapter)
 
 if __name__ == "__main__":
     main()
