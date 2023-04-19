42d8bbc6c0f8837ea3eee2c239c40fbb99d6a75f
purplesquid <purplesquid@users.noreply.github.com>
2023-04-18 23:35:03 -0400

    Sorts numbered jpgs and combines into pdf

--- convertJPGtoCombinedPDF.py
+++ convertJPGtoCombinedPDF.py
@@ -1,5 +1,20 @@
 import os
+import numpy as np
 from PIL import Image
+
+def sortList(path):
+
+    fileArray = []
+    for f in os.listdir(path):
+        fileArray.append(sorted(f.split("."))[0]
+
+    npfileArray = np.array(fileArray)
+                         
+    #Change to int array to sort numbered files
+    result = npfileArray.astype(np.int64)
+    
+    finalSort = sorted(result)
+    return finalSort
 
 def main():
     imagePath = r""
@@ -8,8 +23,8 @@ def main():
     pdfPath = savePath + "\\" + name + ".pdf"
     
     images = [
-        Image.open(imagePath + "\\" + f)
-        for f in os.listdir(imagePath)
+        Image.open(imagePath + "\\" + str(f) + ".JPG")
+        for f in sortList(imagePath)
     ]
 
     images[0].save(
